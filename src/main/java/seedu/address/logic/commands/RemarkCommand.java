9313cb6c6ca67e8a764fdec5c66a9b8c9c907d8a
josh1248 <joshthoo@gmail.com>
2024-10-02 13:44:01 +0800

    Hook RemarkCommand into application

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+/**
+ * Changes the remark of an existing person in the address book.
+ */
+public class RemarkCommand extends Command {
+
+    public static final String COMMAND_WORD = "remark";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult("Hello from remark");
+    }
+}
